9fa5c98e5909e8cb54561d16487d47ecf93456a6
Batrawy0 <ahmedelbatrawy1@icloud.com>
2024-06-03 18:48:51 +0300

    change the return type of getBookById method to be able to return a String message if member doesn't exist

--- src/main/java/com/batrawy/LibraryManagementSystem/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.batrawy.LibraryManagementSystem.repository;
-
-import com.batrawy.LibraryManagementSystem.model.entity.Member;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends CrudRepository<Member, Long> {
-}
