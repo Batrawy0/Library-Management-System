0a2d43e6ef499eb4ce8bd837e08e6e5583aee29c
Batrawy0 <ahmedelbatrawy1@icloud.com>
2024-06-03 14:14:36 +0300

    formatting code

--- src/main/java/com/batrawy/LibraryManagementSystem/model/dto/BookResponse.java
+++ src/main/java/com/batrawy/LibraryManagementSystem/model/dto/BookResponse.java
@@ -11,10 +11,4 @@ public class BookResponse {
     private String title;
     private String author;
     private String isbn;
-
-    /*public BookResponse(String title, String author, String isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }*/
 }
